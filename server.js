2a3f362bfff98a30a2ffdf5ca396d6d045aa194b
Mamadu Billo Djalo <mamadubdj@gmail.com>
2022-06-21 03:16:32 +0100

    mudanca connecao para local

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ dotenv.config({path: './config.env'});
 
 const  DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose.connect(DB,
-        //process.env.DATABASE_LOCAL, => CONEXAO LOCAL
+mongoose.connect(
+      process.env.DATABASE_LOCAL, 
          {})
 .then(conn =>{
         console.log('connection Success');
