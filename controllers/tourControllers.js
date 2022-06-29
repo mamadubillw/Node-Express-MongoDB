61812c0abb57905bbe393d7c8bad67aa37890bfe
Mamadu Billo Djalo <mamadubdj@gmail.com>
2022-06-29 01:36:56 +0100

    Unwinding and Projecting

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -161,11 +161,39 @@ exports.getTourStats = async (req, res) =>{
 
 exports.getMonthlyPlan = async (req, res) =>{
         try {
-                const year = rea.params.year * 1;
-                const plan = await Tour.aggregate([])
+                const year = req.params.year * 1;
+                const plan = await Tour.aggregate([
+
+                        {$unwind: '$startDates'},
+                        {
+                                $match:{
+                                        startDates:{
+                                                $gte: new Date(`${year}-01-01`),
+                                                $lte: new Date(`${year}-12-31`)
+                                        }
+                                }
+                        },
+                        {
+                                $group:{
+                                        _id:{ $month: '$startDates' },
+                                        numTourStarts:{$sum: 1},
+                                        tours:{$push: '$name'}
+                                }
+                        },
+                        {
+                                $addFields:{ month: '$_id' }
+                        },
+                        {
+                                $project:{_id:0}
+                        },
+                        {
+                                $sort:{numTourStarts: -1}
+                        }
+                ]);
                 res.status(200).json({
                         
                         status:'success',
+                       // results: plan.length,
                         data:{
                                 plan
                         }
