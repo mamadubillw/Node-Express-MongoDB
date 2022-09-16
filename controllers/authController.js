eb1d421633ce160a0cd27afb08a412f030224d79
Mamadu Billo Djalo <mamadubdj@gmail.com>
2022-09-16 06:00:01 +0100

    updating the current user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const {promisify} = require('util')
+const {promisify} = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModels');
 const AppError = require('./../utils/appError')
@@ -10,18 +10,32 @@ const signToken = id =>{
         return jwt.sign({id }, process.env.JWT_SECRET, {expiresIn : process.env.JWT_EXPIRES_IN});
 }
 
+const createSendToken = (user, statusCode, res) =>{
+        const token = signToken(user.id);
+
+        res.status(statusCode).json({
+                status: 'success',
+                token,
+                data:{
+                        user
+                }
+        })
+}
+
 exports.signup =catchAsync(async (req, res, next) => {
         const newUser = await User.create(req.body);
-        const token = signToken(newUser._id);
+        createSendToken(newUser, 201, res)
+        // em vez de :
+//         const token = signToken(newUser._id);
         
 
-res.status(201).json({
-        status: 'success',
-        token,
-        data: {
-                user: newUser
-        }
-});
+// res.status(201).json({
+//         status: 'success',
+//         token,
+//         data: {
+//                 user: newUser
+//         }
+// });
 });
 
 exports.login =catchAsync(async (req, res, next) => {
@@ -41,11 +55,8 @@ exports.login =catchAsync(async (req, res, next) => {
                 return next(new AppError ('Incorret Email or Password', 401));
         }
         // 3_if everything is ok send token to client
-        const token = signToken(user._id);
-        res.status(200).json({
-                status: 'success',
-                token
-        });
+        createSendToken(user, 200, res)
+        
 });
 
 exports.protect = catchAsync(async (req, res, next) =>{
@@ -150,10 +161,22 @@ exports.protect = catchAsync(async (req, res, next) =>{
         // 3) Update changedPasswordAt property for the user
 
         // 4) Log the user user in, send JWT
-        const token = signToken(user._id);
-
-        res.status(200).json({
-                status: 'success',
-                token
-        });
+        createSendToken(user, 200, res)
+       
  });
+
+exports.updatePassword = catchAsync(async(req, res, next)=>{
+        //1Get User in collection
+        const user = await User.findById(req.user.id).select('+password');
+        // 2 check if POSTED current password is correct
+         if(!(await user.correctPassword(req.body.passwordCurrent, user.password))){
+                return next(new AppError('Your current password is wrong', 401))
+         }
+
+         //3 if so update password
+         user.password = req.body.password;
+         user.passwordConfirm = req.body.passwordConfirm;
+
+         //4 Log user in send token jwt
+         createSendToken(user, 200, res)        
+})
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,15 @@
+const AppError = require('../utils/appError');
 const User = require('./../models/userModels');
 const catchAsync = require('./../utils/catchAsync');
 
+
+const filterObj = (obj, ...allowedFields) =>{
+        const newObj = {};
+        Object.keys(obj).forEach(el=>{
+                if(allowedFields.includes(el))  newObj[el] = obj[el]
+        });
+        return newObj; 
+}
 
 exports.getAllUsers =catchAsync(async (req, res) =>{
         const users = await User.find();
@@ -26,6 +35,25 @@ exports.getUser = (req, res) =>{
                 message: 'This route is not yet defined'
         });
 };
+
+exports.updateMe = catchAsync(async(req, res, next) =>{
+        //1)-create error if user posted password data
+        if(req.body.password || req.body.passwordConfirm){
+                return next(new AppError('This route is not for password updates. please use /updateMyPassword'))
+        }
+
+        //update user document
+        //filtered out unwanted fields tha are not be allowed to be updated
+        const filteredBody = filterObj(req.body, 'name', 'email');
+        const updateUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {new: true, runValidators:true})
+        //await user.save(); porque
+        res.status(200).json({
+                status: 'success',
+                data:{
+                        user:updateUser
+                }
+        })
+});
 
 exports.updateUser = (req, res) =>{
         res.status(500).json({
