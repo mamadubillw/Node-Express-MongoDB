a430733575686243a8473573979dbe78c1257c0e
Mamadu Billo Djalo <mamadubdj@gmail.com>
2022-09-27 04:48:41 +0100

    data sanitazation

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const morgan = require('morgan'); //middlewere
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
+
+
+//set security http headers
+app.use(helmet());
 //middlewere
 //console.log(process.env.NODE_ENV)
 if(process.env.NODE_ENV === 'development'){
@@ -15,14 +23,33 @@ if(process.env.NODE_ENV === 'development'){
 
 // app.use = global middleware 
 
+//limit requests from same api
 const limiter = rateLimit({
         max:100,
         windowMs:60 * 60 * 1000,
         message: 'tOO many request from this ip, please try again an 1 hour'
 });
 app.use('/api',limiter);
- // that will return one function similar to fuc middlewere      
-app.use(express.json()); // Retorna uma funcao que permitira next req e res
+ // that will return one function similar to func middlewere      
+ //body parser reading data from body into req.body
+app.use(express.json({limit: '10kb'})); // Retorna uma funcao que permitira next req e res
+
+//Data sanitazation against noSql query injection
+app.use(mongoSanitize());
+//Data sanitazation against xss 
+app.use(xss());
+
+//prevent parameter polluition
+app.use(hpp({
+        whitelist: [
+                'duration',
+                'ratingsAverage',
+                'ratingsQuantity',
+                'maxGroupeSize',
+                'difficulty',
+                'price'
+        ]
+}));
 app.use(express.static(`${__dirname}/public`)) // acces to files html
 
 
